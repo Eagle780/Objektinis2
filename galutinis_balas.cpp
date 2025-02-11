0a45069a60a7dba39fd5736ed2111076cd48a26f
Egle <egle.gurklyte@mif.stud.vu.lt>
2025-02-11 22:39:45 +0200

    pridetas pazymiu generavimas su vektoriais

--- galutinis_balas.cpp
+++ galutinis_balas.cpp
@@ -3,45 +3,84 @@
 int main()
 {
     vector<Studentas> A;
-    // Skaityti(n, A);
 
     bool arTesti = true;
     while (arTesti)
     {
         Studentas temp;
         int a;
+        int n = 0;
 
         cout << "Iveskite varda: ";
         cin >> temp.vardas;
         cout << "Iveskite pavarde: ";
         cin >> temp.pavarde;
 
-        cout << "Iveskite pazymius (norint baigti pazymiu rasyma, irasykite 0):" << endl;
-        while (true)
+        string ats = "";
+
+        while (ats != "t" && ats != "n")
         {
-            cin >> a;
-            if (a > 0 && a <= 10)
-            {
-                temp.nd.push_back(a);
-            }
-            else if (a == 0)
-            {
-                if (temp.nd.size() == 0)
-                    cout << "Iveskite bent viena pazymi" << endl;
-                else
-                    break;
-            }
-            else
-                cout << "Neteisingas pazymys" << endl;
+            cout << "Sugeneruoti atsitiktinius pazymius? (t/n)" << endl;
+            cin >> ats;
         }
 
-        a = 0;
-        while (a <= 0 || a > 10)
+        if (ats == "t")
         {
-            cout << "Iveskite egzamino pazymi: ";
-            cin >> a;
+            while (n <= 0)
+            {
+                cout << "Iveskite pazymiu skaiciu: ";
+                cin >> n;
+            }
+
+            srand(time(0));
+
+            for (int i = 0; i < n; i++)
+            {
+                a = 1 + rand() % 10;
+                temp.nd.push_back(a);
+            }
+
+            temp.egz = 1 + rand() % 10;
         }
-        temp.egz = a;
+
+        else
+        {
+            cout << "Iveskite pazymius (norint baigti pazymiu rasyma, irasykite 0):" << endl;
+            while (true)
+            {
+                cin >> a;
+
+                if (cin.fail())
+                {
+                    cout << "Neteisingas ivestis" << endl;
+                    cin.clear();
+                    cin.ignore(1000, '\n');
+                    continue;
+                }
+
+                if (a > 0 && a <= 10)
+                {
+                    temp.nd.push_back(a);
+                }
+                else if (a == 0)
+                {
+                    if (temp.nd.size() == 0)
+                        cout << "Iveskite bent viena pazymi" << endl;
+                    else
+                        break;
+                }
+                else
+                    cout << "Neteisingas pazymys" << endl;
+            }
+
+            a = 0;
+            while (a <= 0 || a > 10)
+            {
+                cout << "Iveskite egzamino pazymi: ";
+                cin >> a;
+            }
+            temp.egz = a;
+        }
 
         A.push_back(temp);
 
