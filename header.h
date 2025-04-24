1095cdc0d08458e55145c885817b4fe54c385a00
Egle <egle.gurklyte@mif.stud.vu.lt>
2025-04-24 20:53:50 +0300

    sutvarkytas studentas.h

--- header.h
+++ header.h
@@ -1,6 +1,7 @@
 #ifndef HEADER_H
 #define HEADER_H
 
+#include "Studentas.h"
 #include <iostream>
 #include <iomanip>
 #include <vector>
@@ -42,77 +43,6 @@ using std::string;
 using std::time;
 using std::to_string;
 using std::vector;
-
-class Studentas
-{
-private:
-    string vardas;
-    string pavarde;
-    vector<int> nd;
-    int egz;
-    int galutinis;
-
-public:
-    Studentas() : egz(0) {}
-    Studentas(string v, string p, vector<int> n, int e)
-    {
-        vardas = v;
-        pavarde = p;
-        nd = n;
-        egz = e;
-    };
-    Studentas(const Studentas &) = default;
-    Studentas &operator=(const Studentas &) = default;
-    Studentas(Studentas &&) = default;
-    Studentas &operator=(Studentas &&) = default;
-    void setVarPav(string v, string p)
-    {
-        vardas = v;
-        pavarde = p;
-    }
-    void setPaz(vector<int> n, int e)
-    {
-        nd = n;
-        egz = e;
-    }
-    void setGal(int g)
-    {
-        galutinis = g;
-    }
-    inline string getVardas() const { return vardas; }
-    inline string getPavarde() const { return pavarde; }
-    inline int getEgz() const { return egz; }
-    inline vector<int> getNd() const { return nd; }
-    inline int getGal() const { return galutinis; }
-    float SkaiciuotiV() const
-    {
-        int s = 0;
-        for (int i = 0; i < nd.size(); i++)
-        {
-            s += nd[i];
-        }
-        float galutinis = 0.4 * (1.0 * s / nd.size()) + 0.6 * egz;
-        return galutinis;
-    }
-    float SkaiciuotiM() const
-    {
-        vector<int> nd2 = nd;
-        float paz;
-        sort(nd2.begin(), nd2.end());
-
-        if (nd2.size() % 2 == 0)
-        {
-            paz = 1.0 * (nd2[nd2.size() / 2 - 1] + nd2[nd2.size() / 2]) / 2;
-        }
-        else
-        {
-            paz = nd2[nd2.size() / 2];
-        }
-
-        float galutinis = 0.4 * paz + 0.6 * egz;
-        return galutinis;
-    }
-};
 
 void cinEx();
 Studentas generuotiPazymius(Studentas temp);
